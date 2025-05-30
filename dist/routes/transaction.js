ff93965fefa51585e16b496069fe3996cf096804
Ernie1234 <anumahjoshuaeneye@gmail.com>
2025-05-30 15:20:56 +0100

    Remove mistakenly committed files

--- dist/routes/transaction.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const transactions_1 = require("../controllers/transactions");
-const router = (0, express_1.Router)();
-// Protected routes
-router.post("/", transactions_1.createTransaction);
-router.get("/:userId", transactions_1.getTransactions);
-exports.default = router;
